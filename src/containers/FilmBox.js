de060077c5a717e4a5e06ee812b08d600c2cafee
Dr-Devine <98554800+Dr-Devine@users.noreply.github.com>
2022-06-14 19:56:38 +0100

    Really bad CSS

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import FilmList from '../components/FilmList';
+import NewReleases from '../components/NewRealeases';
+
+
+
+const FilmBox = () => {
+
+    const [films, setFilms] = useState([
+        {
+          id: 1,
+          name: "Spider-Man: Into the Spider-Verse",
+          url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+        },
+        {
+          id: 2,
+          name: "Life Itself",
+          url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+        },
+        {
+          id: 3,
+          name: "Mary Queen of Scots",
+          url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+        },
+        {
+          id: 4,
+          name: "The Lego Movie 2: The Second Part", 
+          url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+        },
+        {
+          id: 5,
+          name: "Captain Marvel",
+          url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+        }
+      ])
+  
+    return (
+        <>
+            <div className='App'>
+                <h1>Upcoming Film Releases for the UK</h1>
+                    <ul>
+                        <FilmList films={films}/>
+                    </ul>
+                    <section>
+                        <NewReleases/>
+                    </section>
+            </div>
+
+        </>
+    )
+      
+    
+}
+
+export default FilmBox;
